34cae5adea77018a733c37087a1dd6034073816e
niemasd <niemamoshiri@gmail.com>
2019-06-22 22:41:02 -0700

    Addded helper scripts

---
+++ scripts/individual_efficacy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from gzip import open as gopen
+from sys import argv
+if len(argv) != 4:
+    print("USAGE: %s <people_list> <transmission_network> <time>"%argv[0]); exit()
+END = float(argv[3])
+if argv[2].endswith('.gz'):
+    trans = [l.split('\t') for l in gopen(argv[2]).read().decode().strip().splitlines()]
+else:
+    trans = [l.split('\t') for l in open(argv[2]).read().strip().splitlines()]
+if argv[1].endswith('.gz'):
+    people = [l.strip() for l in gopen(argv[1]).read().decode().strip().splitlines()]
+else:
+    people = [l.strip() for l in open(argv[1])]
+num_trans = {u:0 for u in people}
+for u,v,t in trans:
+    if u in num_trans and float(t) > END:
+        num_trans[u] += 1
+for u in people:
+    print('%s\t%d' % (u,num_trans[u]))
---
+++ scripts/optimal_ordering.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from gzip import open as gopen
+from sys import argv
+if len(argv) != 4:
+    print("USAGE: %s <transmission_network> <sample_times> <time>"%argv[0]); exit()
+END = float(argv[3])
+if argv[2].endswith('.gz'):
+    diag_lines = gopen(argv[2]).read().decode().strip().splitlines()
+else:
+    diag_lines = open(argv[2]).read().strip().splitlines()
+diag = {l.split('\t')[0].strip():float(l.split('\t')[1]) for l in diag_lines}
+if argv[1].endswith('.gz'):
+    trans = [l.split('\t') for l in gopen(argv[1]).read().decode().strip().splitlines()]
+else:
+    trans = [l.split('\t') for l in open(argv[1]).read().strip().splitlines()]
+num_trans = {u.split('|')[1]:0 for u in diag if diag[u] <= END}
+for u,v,t in trans:
+    if u in num_trans and float(t) > END:
+        num_trans[u] += 1
+print('\n'.join(sorted(num_trans.keys(), key=lambda x: num_trans[x], reverse=True)))
