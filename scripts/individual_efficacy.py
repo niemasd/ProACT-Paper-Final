7deeefd818fb7e5ba75201b0b922cc6498e48aa7
niemasd <niemamoshiri@gmail.com>
2019-06-28 04:55:43 -0700

    Added check for pipes

--- scripts/individual_efficacy.py
+++ scripts/individual_efficacy.py
@@ -12,9 +12,14 @@ if argv[1].endswith('.gz'):
     people = [l.strip() for l in gopen(argv[1]).read().decode().strip().splitlines()]
 else:
     people = [l.strip() for l in open(argv[1])]
+if '|' in people[0]:
+    people = [u.split('|')[1] for u in people]
 num_trans = {u:0 for u in people}
 for u,v,t in trans:
     if u in num_trans and float(t) > END:
         num_trans[u] += 1
-for u in people:
-    print('%s\t%d' % (u,num_trans[u]))
+try:
+    for u in people:
+        print('%s\t%d' % (u,num_trans[u]))
+except BrokenPipeError:
+    exit()
